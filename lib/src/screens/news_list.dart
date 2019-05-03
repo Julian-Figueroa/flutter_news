0837af02eaaa26200e12b5d895bb0e73c45d568b
Julian Figueroa <julianfigueroatorres@gmail.com>
2019-05-03 18:03:46 -0500

    Adding Bloc Pattern Design

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -6,7 +6,30 @@ class NewsList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: Text('Show some News Here'),
+      body: buildList(),
     );
+  }
+
+  Widget buildList() {
+    return ListView.builder(
+      itemCount: 1000,
+      itemBuilder: (BuildContext context, int index) {
+        return FutureBuilder(
+          future: getFuture(),
+          builder: (BuildContext context, snapshot) {
+            return Container(
+              height: 80.0,
+              child: snapshot.hasData
+                  ? Text('Im visible $index')
+                  : Text('I havent fetch data yet $index'),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  getFuture() {
+    return Future.delayed(Duration(seconds: 2), () => 'Hi');
   }
 }
